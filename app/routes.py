627c2bee6067bff0aa3e15df03fe14c7cb16d227
bajajku <bajajku@sheridancollege.ca>
2024-11-25 19:05:50 -0500

    Added basic expereience to home page for now

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,25 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('index.html', title="Home")
+    experiences = [
+        {
+            "title": "AI and Process Optimization Analyst",
+            "company": "Korah Limited",
+            "period": "Sep 2024 – Present",
+            "details": [
+                "Developed AI algorithms predicting NICU bed availability, reducing transfer decision time by 25%.",
+                "Designed and optimized a SQL database, improving data retrieval speed by 35%.",
+                "Implemented Test-Driven Development (TDD), achieving 100% unit test coverage."
+            ]
+        },
+        {
+            "title": "Artificial Intelligence Research Assistant",
+            "company": "Sheridan Centre for Applied AI",
+            "period": "Oct 2023 – Sep 2024",
+            "details": [
+                "Created a chatbot solving 90% of queries with 40% faster response time.",
+                "Developed Retrieval Augmented Generation (RAG) systems for accuracy improvement."
+            ]
+        }
+    ]
+    return render_template('index.html', title="Home", experiences=experiences)
